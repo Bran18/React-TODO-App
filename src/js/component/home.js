451b60c6cba10e691830f3d9da84d40944381444
Brandon Fernández <andreyfdez18@gmail.com>
2021-03-03 22:09:12 +0000

    adding final CSS file and js handles

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,77 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 export function Home() {
+	const [currentTodo, setCurrentTodo] = useState(""); //declaring the variables for my useState
+	const [list, setList] = useState([
+		{ label: "Become 4Geeks code Jedi", done: false },
+		{ label: "Read more about React", done: true },
+		{ label: "Buy more coffee", done: false },
+		{ label: "Get a new job", done: false },
+		{ label: "Buy a new desktop", done: false }
+	]);
+
+	const handleKeyPress = e => {
+		if (e.key === "Enter") {
+			setList(list.concat({ label: currentTodo, done: false }));
+			setCurrentTodo("");
+		}
+	};
+
+	const deleteTodo = index => setList(list.filter((item, i) => i !== index)); //deleting task by the filter and the id
+
+	const handleCompleteTodo = index => {
+		let newList = [].concat(list);
+		newList[index].done = !newList[index].done;
+
+		setList(newList);
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex flex-column align-items-center justify-content-center wrap">
+			<h1 className="mb-4">My To-Do tracking</h1>
+			<div className="todo-container">
+				<ul className="list-group">
+					<li className="list-group-item">
+						{}
+						<input
+							className="form-control border-0"
+							type="text"
+							placeholder="Something else to do?"
+							aria-label="add todo"
+							value={currentTodo}
+							onChange={e => setCurrentTodo(e.target.value)} //event to add the task
+							onKeyPress={e => handleKeyPress(e)}
+						/>
+					</li>
+					{list.map((item, index) => (
+						<li className="list-group-item" key={index}>
+							<div
+								className={
+									item.done
+										? "status border rounded-circle d-inline-block done mr-3"
+										: "status border rounded-circle d-inline-block mr-3"
+								}
+								onClick={() => handleCompleteTodo(index)}>
+								{" "}
+							</div>
+							{item.label}
+							<span
+								className="delete ml-auto"
+								onClick={() => deleteTodo(index)}>
+								&#10007;
+							</span>
+						</li>
+					))}
+				</ul>
+				<div className="list-group-item footer">
+					{list.length > 0
+						? `${list.length} item${
+								list.length > 1 ? "s" : ""
+						  } left`
+						: "All Done Congratulations! Keep it Up!"}
+				</div>
+			</div>
 		</div>
 	);
 }
